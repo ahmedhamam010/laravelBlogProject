1d2607f1867cc6d866025b4efae0a83605411b75
ahmedhamam010 <ahmedhamam454546>
2020-01-07 12:30:01 +0200

    fix github error and remove hashed code

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -58,27 +58,28 @@ class LoginController extends Controller
     {
         // dd(Socialite::driver($driver)->user());
 
-        // try {
-        //     $user = Socialite::driver($driver)->user();
-        // } catch (\Exception $e) {
-        //     return redirect()->route('login');
-        // }
-        // $existingUser = User::where('email', $user->getEmail())->first();
-        // if ($existingUser) {
-        //     auth()->login($existingUser, true);
-        // } else {
-        //     $newUser                    = new User;
-        //     $newUser->provider_name     = $driver;
-        //     $newUser->provider_id       = $user->getId();
-        //     $newUser->name              = $user->nickname();
-        //     $newUser->email             = $user->getEmail();
-        //     $newUser->email_verified_at = now();
-        //     $newUser->save();
+        try {
+            $user = Socialite::driver($driver)->user();
+        } catch (\Exception $e) {
+            return redirect()->route('login');
+        }
+        $existingUser = User::where('email', $user->getEmail())->first();
+        $userName = $user->getName()? $user->getName():$user->nickname;
+        if ($existingUser) {
+            auth()->login($existingUser, true);
+        } else {
+            $newUser                    = new User;
+            $newUser->provider_name     = $driver;
+            $newUser->provider_id       = $user->getId();
+            $newUser->name              = $userName;
+            $newUser->email             = $user->getEmail();
+            $newUser->email_verified_at = now();
+            $newUser->save();
     
-        //     auth()->login($newUser, true);
-        // }
+            auth()->login($newUser, true);
+        }
     
-        // return redirect($this->redirectPath());
+        return redirect($this->redirectPath());
     
 
     }
